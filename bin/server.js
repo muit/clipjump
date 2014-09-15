1a06a9cba2a2498d08dc6dfd6938fa034988afea
muit <miguel_3c@hotmail.com>
2014-09-15 20:20:44 +0200

    Preparing

---
+++ bin/server.js
@@ -0,0 +1,15 @@
+if(typeof Utyl == "undefined") require("../source/utyl/utyl.js");
+http = require("../source/http_server.js");
+
+//*******************************
+// HTTP SERVER
+// Only server the html & other files
+//*******************************
+var port = 3000;
+var options = {
+    home: "index.html",
+    debug: true,
+    secure: false
+};
+
+webServer = http.start(port, options);
