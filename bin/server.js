87e87dc0ed2d678ac2ab4fdde31969568040a6c5
muit <miguel_3c@hotmail.com>
2014-10-29 22:16:43 +0100

    Updated ServeMe & implemented as package

--- bin/server.js
+++ bin/server.js
@@ -6,17 +6,18 @@
  ****************************************************************************/
 
 if(typeof Utyl == "undefined") require("../source/utyl/utyl.js");
-http = require("../source/serveMe/source/http_server.js");
 
-//*******************************
-// HTTP SERVER
-// Only server the html & other files
-//*******************************
-var port = 80;
+/*******************************
+ * HTTP SERVER
+********************************/
+var port = 8080;
 var options = {
     home: "index.html",
+    directory: "./public",
     debug: true,
     secure: false
 };
 
-webServer = http.start(port, options);
+var ServeMe = require("serve-me")(options);
+
+var webServer = ServeMe.start(port);
