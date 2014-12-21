1d315d428e4ff8ad0d3c4eca596da1a820adbc85
muit <miguel_3c@hotmail.com>
2014-12-21 19:29:15 +0100

    Editor: Asset Explorer almost working

--- public/editor/assets/cj-editor.js
+++ public/editor/assets/cj-editor.js
@@ -15,7 +15,101 @@ CJ.Editor.prototype.loadComponents = function()
 
   this.assetExplorer   = {
     element: document.getElementById("assetexplorer"),
+    _empty_container: document.getElementById("component-1028"),
+    _container: document.getElementById("dataview-1029"),
+    _showing: [],
+    _showing_type: "all",
+
+    cubes: [],
+    materials: [],
+    maps: [],
+
+    init: function(){
+      this._empty_container.innerHTML = CJ.Editor.Text.ASSET_EXPLORER_EMPTY;
+      this._show();
+    },
+
+    showAll: function(){
+      this._showing_type = "all";
+      this._show();
+    },
+
+    showCubes: function(){
+      this._showing_type = "cubes";
+      this._show();
+    },
+
+    showMaterials: function(){
+      this._showing_type = "materials";
+      this._show();
+    },
+
+    showMaps: function(){
+      this._showing_type = "maps";
+      this._show();
+    },
+
+    _show: function(){
+      this._reset();
+      var i, len, unit;
+
+      if(this._showing_type == "cubes" || this._showing_type == "all")
+        for(i = 0, len = this.cubes.length; i<len; i++){
+          unit = {
+            name: this.cubes[i].name,
+            element: CJ.Editor.Text.get("ASSET_EXPLORER_CUBE", {id: i, name: this.cubes[i].name})
+          };
+          this._showing.push(unit);
+          this._container.innerHTML += unit.element;
+        }
+
+      if(this._showing_type == "materials" || this._showing_type == "all")
+        for(i = 0, len = this.materials.length; i<len; i++){
+          unit = {
+            name: this.cubes[i].name,
+            element: CJ.Editor.Text.get("ASSET_EXPLORER_MATERIAL", {id: i, name: this.materials[i].name})
+          };
+          this._showing.push(unit);
+          this._container.innerHTML += unit.element;
+        }
+
+      if(this._showing_type == "maps" || this._showing_type == "all")
+        for(i = 0, len = this.maps.length; i<len; i++){
+          unit = {
+            name: this.cubes[i].name,
+            element: CJ.Editor.Text.get("ASSET_EXPLORER_MAP", {id: i, name: this.maps[i].name})
+          };
+          this._showing.push(unit);
+          this._container.innerHTML += unit.element;
+        }
+
+      if(this._showing.length < 1)
+        this._showEmpty();
+    },
+
+    _showEmpty: function(){
+      this._empty_container.style.display = "block";
+    },
+    _reset: function(){
+      this._showing = [];
+      this._empty_container.style.display = "none";
+      this._container.innerHTML = "";
+    },
+
+    addCube: function(asset){
+      this.cubes.push(asset);
+      this._show();
+    },
+    addMaterial: function(asset){
+      this.cubes.push(asset);
+      this._show();
+    },
+    addMap: function(asset){
+      this.maps.push(asset);
+      this._show();
+    }
   };
+  this.assetExplorer.init();
 
   this.attributeEditor = {
     element: document.getElementById("east-region-container"),
@@ -42,6 +136,22 @@ CJ.Editor.prototype.loadComponents = function()
       this.loading.text.innerHTML = (value)?text:"";
     },
   };
+};
+
+CJ.Editor.Text = {
+  get: function(name, attrs){
+    var text = this[name];
+    if(attrs && typeof attrs == "object")
+      for(rep in attrs){
+        text = text.replace("%"+rep+"%", attrs[rep]);
+      }
+    return text;
+  },
+
+  ASSET_EXPLORER_EMPTY:    'No assets available. Drag and drop some assets here.',
+  ASSET_EXPLORER_CUBE:     '<div class="pcd-dataview-item"><img src="./assets/images/icons/64x64/asset.png"><span class="pcd-dataview-item-label" id="%id%">%name%</span></div>',
+  ASSET_EXPLORER_MATERIAL: '<div class="pcd-dataview-item"><img src="./assets/images/icons/64x64/material.png"><span class="pcd-dataview-item-label" id="%id%">%name%</span></div>',
+  ASSET_EXPLORER_MAP:      '<div class="pcd-dataview-item"><img src="./assets/images/icons/64x64/asset.png"><span class="pcd-dataview-item-label" id="%id%">%name%</span></div>',
 };
 
 
